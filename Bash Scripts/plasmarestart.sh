6b04c68c9c29f7c471181e960eae599cb32ac833
Joshua Zavala <104445379+joshzavala@users.noreply.github.com>
2022-05-09 13:09:57 -0500

    Update plasmarestart.sh

--- Bash Scripts/plasmarestart.sh
+++ Bash Scripts/plasmarestart.sh
@@ -1,7 +1,11 @@
+# small script with commands to kill plasmashell and restart it if plasma was acting slow or buggy.
+
 #!/bin/bash
 
+# kills all plasmashell processes running
 killall plasmashell
 
 sleep 1
 
+# starts all plasmshell processes fresh again
 kstart5 plasmashell
