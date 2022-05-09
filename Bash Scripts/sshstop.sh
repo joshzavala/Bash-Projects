cec180588e160f7efc23848f98dd3fa2d030dec0
Joshua Zavala <104445379+joshzavala@users.noreply.github.com>
2022-05-09 13:12:54 -0500

    Update sshstop.sh

--- Bash Scripts/sshstop.sh
+++ Bash Scripts/sshstop.sh
@@ -1,7 +1,12 @@
+# Small commands to stop openssh.
+# I use it to auto stop the serive when I log out of my computer.
+
 #!/bin/bash
 
+# stopping openssh
 sudo systemctl stop sshd.service
 
 sleep 1
 
+# status of openssh
 sudo systemctl status sshd.service
