4c7a01b3a08c73f1e3b18757c17dfd24e814221f
Joshua Zavala <104445379+joshzavala@users.noreply.github.com>
2022-05-09 13:11:16 -0500

    Update sshstart.sh

--- Bash Scripts/sshstart.sh
+++ Bash Scripts/sshstart.sh
@@ -1,7 +1,11 @@
+# Simple commands to start openssh and output its status to confirm it active.
+
 #!/bin/bash
 
+# starting openssh
 sudo systemctl start sshd.service
 
 sleep 1
 
+# status of openssh
 sudo systemctl status sshd.service
